3f842e93a9d546a4faeb08358920c60efb9038a6
jakobsvenningsson <jsvennin@cisco.com>
2022-12-02 16:08:10 +0100

    add tests

--- Makefile
+++ Makefile
@@ -1,4 +1,14 @@
+DAYS = $(wildcard day*)
+
 all: common/ebin/common.beam
 
 common/ebin/common.beam: common/common.erl
 	erlc -o $(@D) $^
+
+test:
+	@$(foreach DAY,$(DAYS),./test.bash $(DAY);)
+
+clean:
+	rm -f common/ebin/*
+
+.PHONY: test clean
---
+++ test.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+day=$1
+
+(cd $day;
+ printf "$day:\n"
+ for test in 1 2; do
+     if [ "$(${test}.escript)" = "$(cat expect/${test})" ]; then
+         color="\e[1;32m"
+         res="ok"
+     else
+         color="\e[1;31m"
+         res="failed"
+     fi
+     printf "  test $test: $color%s\e[1;0m\n" "$res"
+ done
+)
