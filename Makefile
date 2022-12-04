0651322e288d33a3816710376069c3fcc3127786
jakobsvenningsson <jsvennin@cisco.com>
2022-12-04 18:35:35 +0100

    update makefile and test script

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
 DAYS = $(wildcard day*)
 
-all: build
+all: build test
 
 build: common/ebin/common.beam
 
 common/ebin/common.beam: common/common.erl
 	erlc -o $(@D) $^
+
+test-day%:
+	@./test.bash $*
 
 test:
 	@$(foreach DAY,$(DAYS:day%=%),./test.bash $(DAY);)
--- test.bash
+++ test.bash
@@ -7,13 +7,21 @@ day=$1
 (cd day${day};
  printf "day ${day}:\n"
  for test in 1 2; do
-     if [ "$(${test}.escript)" = "$(cat expect/${test})" ]; then
-         color="\e[1;32m"
-         res="ok"
-     else
-         color="\e[1;31m"
-         res="failed"
-     fi
-     printf "  test $test: $color%s\e[1;0m\n" "$res"
+     printf "  test $test:\n"
+     for input in input_sample input; do
+         if [ ! -f "data/${input}.txt" ] || [ ! -f "expect/${test}/${input}.txt" ]; then
+             color="\e[1;35m"
+             res="not run"
+         else
+             if [ "$(${test}.escript data/${input}.txt 2> /dev/null)" = "$(cat expect/${test}/${input}.txt)" ]; then
+                 color="\e[1;32m"
+                 res="ok"
+             else
+                 color="\e[1;31m"
+                 res="failed"
+             fi
+         fi
+         printf "    $input: $color%s\e[1;0m\n" "$res"
+     done
  done
 )
