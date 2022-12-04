6b52ce80ee3b4ac83eee4bbe1f9c534a72279c9e
jakobsvenningsson <jsvennin@cisco.com>
2022-12-04 18:36:15 +0100

    update common

--- common/common.erl
+++ common/common.erl
@@ -2,11 +2,26 @@
 
 -export([input/1]).
 -export([input/2]).
+-export([input/3]).
 
 input(Path) ->
-    input(Path, fun(X) -> X end).
+    input(Path, <<"\n">>).
 
-input(Path, F) ->
+input(Path, F) when is_function(F) ->
+    input(Path, <<"\n">>, F);
+input(Path, Delim) ->
+    input(Path, Delim, fun(X) -> X end).
+
+input(Path, Delim, F) ->
     {ok, Data} = file:read_file(Path),
-    Lines = string:split(string:trim(Data), <<"\n">>, all),
-    lists:map(F, Lines).
+    Lines = binary:split(Data, Delim, [global, trim]),
+    lists:filtermap(fun(Line) ->
+                            case F(Line) of
+                                {true, _} = Res ->
+                                    Res;
+                                false ->
+                                    false;
+                                Res ->
+                                    {true, Res}
+                            end
+                    end, Lines).
